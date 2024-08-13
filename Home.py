5f3eae321b34e55290b1ad1e4daa8f8cfc494a0f
Shafin <bci70giii@mozmail.com>
2024-08-13 15:10:52 +0600

    It will be online

---
+++ Home.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")  # sets the website to be wide
+
+col1, col2 = st.columns(2)  # divides the page into 2 columns
+
+with col1:  # treated as a file. we are working on column 1
+    st.image("images/photo.png")  # shows the indicated image on the page.
+
+with col2:
+    st.title("Ahnaf Hasan Shafin")  # title on the page
+    content = """Hi this is Shafin. Introduction will be added later."""    # Contains
+    # multiline text
+
+    st.info(content)    # can use st.write but st.info produces bluish box, looks cool
+
+st.write("Below you can see a few apps that I tried to build.")
+
+col3,empty_column, col4 = st.columns([1.5,0.5,1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:5].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('Images/' + row['image'])
+
+
+with col4:
+    for index, row in df[5:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('Images/' + row['image'])
---
+++ pages/Contact_Me.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.header("Contact Me")
+
+with st.form(key="email"):
+    user_email = st.text_input("Enter your email address here.")
+    raw_message = st.text_area("Your message")
+    message =f"""\
+    Subject: New email from {user_email}
+    
+    From: {user_email}
+    {raw_message}
+    """
+    button = st.form_submit_button("Submit")
+    if button:
+        st.info("Your email was sent successfully!")
+
