ed59fd29c03ed53adb1cc7a11868bd3a79e04474
Shafin <bci70giii@mozmail.com>
2024-08-17 17:34:28 +0600

    It will be online

--- Home.py
+++ Home.py
@@ -10,12 +10,16 @@ with col1:  # treated as a file. we are working on column 1
 
 with col2:
     st.title("Ahnaf Hasan Shafin")  # title on the page
-    content = """Hi this is Shafin. Introduction will be added later."""    # Contains
+    content = """Hello, I’m Ahnaf Hasan Shafin, a dedicated Mechatronics Engineering student from KUET. As I pursue 
+    my studies, I’m constantly exploring and honing my skills in programming, embedded systems, robotics, 
+    product design, and more. My curiosity and drive to learn have led me to work on various projects, and I'm eager 
+    to apply my knowledge to solve real-world challenges and contribute meaningfully to the field."""    #
+    # Contains
     # multiline text
 
     st.info(content)    # can use st.write but st.info produces bluish box, looks cool
 
-st.write("Below you can see a few apps that I tried to build.")
+st.write("Below you can see a few projects that I worked on.")
 
 col3,empty_column, col4 = st.columns([1.5,0.5,1.5])
 
