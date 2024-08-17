de87ecead4b0e07bca547915e0fe9a2af9847838
Shafin <bci70giii@mozmail.com>
2024-08-17 17:00:41 +0600

    It will be online

--- send_email.py
+++ send_email.py
@@ -1,12 +1,13 @@
 import smtplib, ssl
+import streamlit as st
 import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = os.getenv("db_username")
-    password = os.getenv("db_password")
+    username = st.secrets("db_username")
+    password = st.secrets("db_password")
     receiver = "muffinshafin@gmail.com"
     my_context = ssl.create_default_context()  # to send email securely
 
