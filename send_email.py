142b13d402631f3c2dd4a8b2d168755e25724aad
Shafin <bci70giii@mozmail.com>
2024-08-17 17:21:43 +0600

    It will be online

--- send_email.py
+++ send_email.py
@@ -5,8 +5,8 @@ import os
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
-    username = st.write(st.secrets["db_username"])  # Corrected to use square brackets
-    password = st.write(st.secrets["db_password"])  # Corrected to use square brackets
+    username = str(st.secrets["db_username"])  # Corrected to use square brackets
+    password = str(st.secrets["db_password"])  # Corrected to use square brackets
     receiver = "muffinshafin@gmail.com"
     my_context = ssl.create_default_context()  # to send email securely
 
